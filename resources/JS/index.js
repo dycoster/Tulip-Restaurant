9e680ce6616b55c0bfedf111ed8b155ebedb3230
dycoster <61963464+dycoster@users.noreply.github.com>
2023-08-23 13:33:08 +1000

    animate images

--- resources/JS/index.js
+++ resources/JS/index.js
@@ -37,3 +37,19 @@ const navSlide = () => {
 
 }
 navSlide();
+
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('image-animation');
+      }
+    });
+  });
+  
+  const viewbox = document.querySelectorAll('.image');
+  viewbox.forEach(image => {
+    observer.observe(image);
+  });
